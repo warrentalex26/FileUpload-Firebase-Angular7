afc5b1c978af27548cd5f2e62025ee9d9df97ba1
Alexander Talavera <warrentalex26ies@gmail.com>
2019-03-14 16:44:53 -0600

    obtener archivos y validarlos

--- src/app/directives/ng-drop-files.directive.ts
+++ src/app/directives/ng-drop-files.directive.ts
@@ -14,6 +14,7 @@ export class NgDropFilesDirective {
   @HostListener('dragover', ['$event'])
   public onDragEnter(event: any) {
     this.mouseSobre.emit(true);
+    this._prevenirDetener();
   }
 
   @HostListener('dragleave', ['$event'])
@@ -21,9 +22,37 @@ export class NgDropFilesDirective {
     this.mouseSobre.emit(false);
   }
 
+  @HostListener('drop', ['$event'])
+  public onDrop(event: any) {
+    const transferencia = this._getTransferencia(event); // Ya obtenemos la informacion de los archivos, eso vendria del dataTransfer pero extendemos mejor la compatibilidad
+    if (!transferencia) { // Sino hay transferencia no hacemos nada
+      return;
+    }
+    this._extraerArchivos(transferencia.files); // Los archivos se encuentran en la transferencia file
+    this._prevenirDetener(); // Para que no lo abra aun
+    this.mouseSobre.emit(false);
+  }
+
+  private _getTransferencia(event: any) {
+    return event.dataTransfer ? event.dataTransfer : event.originalEvent.dataTransfer; // Extendemos la compatibilidad de navegadores
+  }
+
+  private _extraerArchivos(archivosLista: FileList) {
+    console.log(archivosLista);
+    // Extraemos el objeto y lo pasamos a un arreglo
+    for (const propiedad in Object.getOwnPropertyNames(archivosLista)) {
+      const archivoTemporal = archivosLista[propiedad];
+      if (this._archivoPuedeSerCargado(archivoTemporal)) { // Verificamos si el archivo puede ser cargado
+        const nuevoArchivo = new FileItem(archivoTemporal); // Seteamos las propiedades para que sean guardadas en el nuevo archivo
+        this.archivos.push(nuevoArchivo); // La insertamos en el arreglo
+      }
+    }
+    console.log(this.archivos);
+  }
+
   // Validaciones
 
-  private _archivoPuedeSerAcargado(archivo: File): boolean { // Verificamos su el archivo fue dropeado y si es una imagen
+  private _archivoPuedeSerCargado(archivo: File): boolean { // Verificamos su el archivo fue dropeado y si es una imagen
     if (!this._archivoYaFueDroppeado(archivo.name) && this._esImagen(archivo.type)) { // this._esImagen(archivo.type) ahi viene el tipo de archivo
       return true;
     } else {
@@ -39,7 +68,7 @@ export class NgDropFilesDirective {
   private _archivoYaFueDroppeado(nombreArchivo: string): boolean { // para saber si el archivo existe en el arreglo
     for (const archivo of this.archivos) {
       if (archivo.nombreArchivo === nombreArchivo) {
-        console.log('El archivo' + nombreArchivo + 'ya esta agregado');
+        console.log('El archivo' + nombreArchivo + ' ya esta agregado');
         return true;
       }
     }
